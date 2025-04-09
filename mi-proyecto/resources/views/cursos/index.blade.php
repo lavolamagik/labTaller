aea7941bd24d53ed5752676abc81bfd9c8842638
Angel <abravo21@alumnos.utalca.cl>
2025-04-09 11:05:23 -0400

    prueba

--- mi-proyecto/resources/views/cursos/index.blade.php
+++ mi-proyecto/resources/views/cursos/index.blade.php
@@ -10,22 +10,21 @@
 
         <div class="group" >
         <form  method="GET" action="{{ route('cursos.search') }}">
-        <svg viewBox="0 0 24 24" aria-hidden="true" class="search-icon">
-            <g>
-            <path
-                d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"
-            ></path>
-            </g>
-        </svg>
-
-        <input
-            id="search"
-            class="input"
-            type="search"
-            placeholder="Ordenar"
-            name="search"
-            value="{{ request('search') }}"
-        />
+        <h2 class="text-center display-4">Search</h2>
+            <div class="row">
+                <div class="col-md-8 offset-md-2">
+                    <form action="simple-results.html">
+                        <div class="input-group">
+                            <input type="search" class="form-control form-control-lg" placeholder="Type your keywords here">
+                            <div class="input-group-append">
+                                <button type="submit" class="btn btn-lg btn-default">
+                                    <i class="fa fa-search"></i>
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
         
         <button type="submit">Buscar</button>
         </form>
